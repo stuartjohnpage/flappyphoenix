d3a773b738cf1629e8d4d4ba4cc64ea44f739499
Revelry <stuartjohnpage@live.com>
2024-10-18 00:11:11 -0500

    first mobile mobes

--- lib/flappy/player.ex
+++ lib/flappy/player.ex
@@ -1,7 +1,7 @@
 defmodule Flappy.Player do
   @moduledoc """
   A struct to represent a player in the game.
-  It contains the position and velocity of the enemy.
+  It contains the position and velocity of the player.
   The position is a tuple of x and y coordinates, and the velocity is a tuple of x and y velocities.
   """
   defstruct position: {0, 0, 0, 0}, velocity: {0, 0}, sprite: %{image: "", size: {0, 0}, name: :atom}, id: ""
